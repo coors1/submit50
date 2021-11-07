9d2c82a68b1a95e4cbf418d3363ba307b0fe804b
coors1 <90646551+coors1@users.noreply.github.com>
2021-11-06 21:32:42 -0400

    Create mario.c
    // Prints a 3-by-3 grid of bricks with nested loops
      #include <cs50.h>
     #include <stdio.h>
     
      int main (void)
     
    {  for (int i = 1; i < 8 ;i++)
      { for (int j = 0; j < 9 ;j++)
     {
     printf("#");
     }
     printf("\n");
    }
         }

---
+++ mario.c
@@ -0,0 +1,14 @@
+// Prints a 3-by-3 grid of bricks with nested loops
+  #include <cs50.h>
+ #include <stdio.h>
+ 
+  int main (void)
+ 
+{  for (int i = 1; i < 8 ;i++)
+  { for (int j = 0; j < 9 ;j++)
+ {
+ printf("#");
+ }
+ printf("\n");
+}
+     }
